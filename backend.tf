c4b9b9778bfe15bada136b6d3645d12d3288d883
ahkerarhaheed <ahkera.rhaheed-muhammad@cloudreach.com>
2022-03-28 14:10:11 -0400

    Setting up backend to work with terraform

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "talent-academy-5609802262-tfstates"
     key            = "sprint2/week1/training-terraform/terraform.tfstates"
-
+     dynamodb_table = "terraform-lock"
   }
 }
